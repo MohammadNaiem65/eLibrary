8b58f47c8aeb4be4b4709c2bc6f858fed1f5b50c
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-12-14 16:28:28 +0600

    add: configured tanstack query's query client

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import Sidebar from './shared/Sidebar';
 import Loader from './shared/Loader';
 
 function App() {
     return (
-        <main
-            className='font-host flex items-start justify-between'
-            style={{
-                background:
-                    'linear-gradient(90deg, rgba(241,238,227,1) 0%, rgba(241,238,227,1) 18%, rgba(253,253,246,1) 18%, rgba(253,253,246,1) 100%)',
-            }}
-        >
-            <Sidebar />
-
-            <main className='w-[82%] min-h-screen ml-auto pl-5'>
-                <Suspense fallback={<Loader />}>
-                    <Outlet />
-                </Suspense>
-            </main>
+        <main className='min-h-screen font-host bg-[rgba(253,253,246,1)]'>
+            <Suspense fallback={<Loader />}>
+                <Outlet />
+            </Suspense>
         </main>
     );
 }
