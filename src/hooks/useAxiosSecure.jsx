c9452cf6d39a2cf9ed930a923c02548f40e18fda
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-12-17 16:45:29 +0600

    update: updated the baseURL of useAxiosSecure hook

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosSecure = axios.create({
-    baseURL: 'https://openlibrary.org',
+    baseURL: 'http://localhost:3000',
 });
 
 export default function useAxiosSecure() {
