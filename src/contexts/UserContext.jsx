7ecdb72d790235e4c0cccb3b6b8971ad4497bcba
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-12-19 19:40:50 +0600

    add: created UserContext and set up at main.jsx

---
+++ src/contexts/UserContext.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { createContext } from 'react';
+
+const UserContext = createContext();
+
+export const UserContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+export default UserContext;
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,21 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { UserContextProvider } from './contexts/UserContext.jsx';
 import router from './router/router.jsx';
 import './index.css';
+import { AuthContextProvider } from './contexts/AuthContext.jsx';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
-        </QueryClientProvider>
+        <AuthContextProvider>
+            <UserContextProvider>
+                <QueryClientProvider client={queryClient}>
+                    <RouterProvider router={router} />
+                </QueryClientProvider>
+            </UserContextProvider>
+        </AuthContextProvider>
     </StrictMode>
 );
