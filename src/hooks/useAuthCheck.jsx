3b0ca5933d1aa4fa6cd32738acddb81695887e1f
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-12-19 19:40:05 +0600

    add: created useAuthCheck hook

---
+++ src/hooks/useAuthCheck.jsx
@@ -0,0 +1,23 @@
+import { useContext } from 'react';
+import { useState, useEffect } from 'react';
+import UserContext from '../contexts/UserContext';
+import AuthContext from '../contexts/AuthContext';
+
+export default function useAuthCheck() {
+    const [authChecked, setAuthChecked] = useState(false);
+    const { setUser } = useContext(UserContext);
+    const { setAuthData } = useContext(AuthContext);
+
+    useEffect(() => {
+        const { accessToken, user } = JSON.parse(localStorage.getItem('auth'));
+
+        if (user && accessToken) {
+            setUser(user);
+            setAuthData({ accessToken });
+        }
+
+        setAuthChecked(true);
+    }, [setUser, setAuthData]);
+
+    return authChecked;
+}
